d33bce1cccdb449d102d8022194a97e5e0727f04
Maxim Egorov <abd.andrew@gmail.com>
2024-05-14 13:08:08 +0500

    Add vector types

---
+++ src/vector.h
@@ -0,0 +1,17 @@
+#ifndef VECTOR_H_
+#define VECTOR_H_
+
+typedef struct {
+    float x;
+    float y;
+} vec2_t;
+
+typedef struct {
+    float x;
+    float y;
+    float z;
+} vec3_t;
+
+// TODO: Add functions to manipulate vectors 2D and 3D
+
+#endif // VECTOR_H_
